3ee4999da5d3ac5cf6cecc44fafad4855e41e2a6
latenightdev <latenightdev@gmail.com>
2021-12-15 14:31:25 -0600

    add auth guard back

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { AuthorizationGuard } from './services/authorization.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthorizationGuard] },
   { path: 'create', component: CreateComponent, canActivate: [AuthorizationGuard] },
   { path: 'view', component: ViewComponent, canActivate: [AuthorizationGuard] },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
